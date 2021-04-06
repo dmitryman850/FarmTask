9fbbae46e8c07fdd1b340f7311c0caf9909238d5
Dmitry Man <dmitryman850@gmail.com>
2021-04-06 23:43:18 +0300

    add Start Fragment

---
+++ app/src/main/java/dmitry/man/farmapplication/StartFragment.kt
@@ -0,0 +1,34 @@
+package dmitry.man.farmapplication
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class StartFragment: Fragment() {
+    private var text : TextView ? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text = view.findViewById(R.id.tv_start_title)
+    }
+
+    companion object {
+        fun newInstance() = StartFragment()
+    }
+}
