d2edc7b3434d237df1c34aa0a56ba8ec3005eb67
Dmitry Man <dmitryman850@gmail.com>
2021-04-11 19:31:00 +0300

    add SharedPreferences

--- app/src/main/java/dmitry/man/farmapplication/MainActivity.kt
+++ app/src/main/java/dmitry/man/farmapplication/MainActivity.kt
@@ -1,22 +1,58 @@
 package dmitry.man.farmapplication
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), StartFragmentClickListener {
+class MainActivity : AppCompatActivity(),
+    StartFragmentClickListener {
+
+    private var savedSettingsStart: SharedPreferences? = null
+    private val PREFS_FILE = "Account"
+    private val PREF_OPERATOR = "Operator"
+    private val PREF_CUSTOMER = "Customer"
+    private val PREF_FARM = "Farm"
+    private val PREF_FIELD = "Field"
+    private var mainIdOperator: Int = 0
+    private var mainIdCustomer: Int = 0
+    private var mainIdFarm: Int = 0
+    private var mainIdField: Int = 0
+    private var prefEditor: SharedPreferences.Editor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedSettingsStart = getSharedPreferences(PREFS_FILE, MODE_PRIVATE)
+        getSettings()
 
         supportFragmentManager.beginTransaction()
-            .add(R.id.container_activity_main, StartFragment.newInstance())
+            .add(
+                R.id.container_activity_main,
+                StartFragment.newInstance(mainIdOperator, mainIdCustomer, mainIdFarm, mainIdField)
+            )
             .commit()
     }
 
-    override fun save() {
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun save(idOperator: Int, idCustomer: Int, idFarm: Int, idField: Int) {
+        prefEditor = savedSettingsStart!!.edit()
+        prefEditor?.putInt(PREF_OPERATOR, idOperator)
+        prefEditor?.putInt(PREF_CUSTOMER, idCustomer)
+        prefEditor?.putInt(PREF_FARM, idFarm)
+        prefEditor?.putInt(PREF_FIELD, idField)
+        prefEditor?.apply()
+
         val intent = Intent(this, Main2Activity::class.java)
         startActivity(intent)
+    }
+
+    fun getSettings() {
+        mainIdOperator = savedSettingsStart?.getInt(PREF_OPERATOR, 0) ?: 0
+        mainIdCustomer = savedSettingsStart?.getInt(PREF_CUSTOMER, 0) ?: 0
+        mainIdFarm = savedSettingsStart?.getInt(PREF_FARM, 0) ?: 0
+        mainIdField = savedSettingsStart?.getInt(PREF_FIELD, 0) ?: 0
     }
 }
--- app/src/main/java/dmitry/man/farmapplication/StartFragment.kt
+++ app/src/main/java/dmitry/man/farmapplication/StartFragment.kt
@@ -6,11 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class StartFragment : Fragment() {
     private var startFragmentClickListener: StartFragmentClickListener? = null
     private var btnSave: Button? = null
+    private var radioGroupOperator: RadioGroup? = null
+    private var radioGroupCustomer: RadioGroup? = null
+    private var radioGroupFarm: RadioGroup? = null
+    private var radioGroupField: RadioGroup? = null
+    private var idRadioGroupOperator: Int = 0
+    private var idRadioGroupCustomer: Int = 0
+    private var idRadioGroupFarm: Int = 0
+    private var idRadioGroupField: Int = 0
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -30,9 +41,128 @@ class StartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnSave = view.findViewById<Button>(R.id.btn_start_save)
+        val b: Bundle? = arguments
+        idRadioGroupOperator = b?.getInt("Operator", 0) ?: 0
+        idRadioGroupCustomer = b?.getInt("Customer", 0) ?: 0
+        idRadioGroupFarm = b?.getInt("Farm", 0) ?: 0
+        idRadioGroupField = b?.getInt("Field", 0) ?: 0
+
+        when (idRadioGroupOperator) {
+            0 -> {
+                val nameOperator = view.findViewById<RadioButton>(R.id.r_button_name_of_operator)
+                nameOperator.isChecked = true
+            }
+            1 -> {
+                val otherName = view.findViewById<RadioButton>(R.id.r_button_other_name)
+                otherName.isChecked = true
+            }
+            else -> {
+            }
+        }
+        when (idRadioGroupCustomer) {
+            0 -> {
+                val nameOfCustomer = view.findViewById<RadioButton>(R.id.r_button_name_of_customer)
+                nameOfCustomer.isChecked = true
+            }
+            1 -> {
+                val anotherNameOf = view.findViewById<RadioButton>(R.id.r_button_another_name_of)
+                anotherNameOf.isChecked = true
+            }
+            else -> {
+            }
+        }
+        when (idRadioGroupFarm) {
+            0 -> {
+                val farmName = view.findViewById<RadioButton>(R.id.r_button_farm_name)
+                farmName.isChecked = true
+            }
+            1 -> {
+                val nameOfAFarm = view.findViewById<RadioButton>(R.id.r_button_name_of_a_farm)
+                nameOfAFarm.isChecked = true
+            }
+            else -> {
+            }
+        }
+        when (idRadioGroupField) {
+            0 -> {
+                val fieldName = view.findViewById<RadioButton>(R.id.r_button_field_name)
+                fieldName.isChecked = true
+            }
+            1 -> {
+                val nameOfAField = view.findViewById<RadioButton>(R.id.r_button_name_of_a_field)
+                nameOfAField.isChecked = true
+            }
+            2 -> {
+                val fieldN1 = view.findViewById<RadioButton>(R.id.r_button_field_n1)
+                fieldN1.isChecked = true
+            }
+            else -> {
+            }
+        }
+        btnSave = view.findViewById(R.id.btn_start_save)
         btnSave?.setOnClickListener {
-            startFragmentClickListener?.save()
+            startFragmentClickListener?.save(
+                idRadioGroupOperator,
+                idRadioGroupCustomer,
+                idRadioGroupFarm,
+                idRadioGroupField
+            )
+        }
+        radioGroupOperator = view.findViewById(R.id.r_group_operator)
+        radioGroupOperator?.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.r_button_name_of_operator -> {
+                    idRadioGroupOperator = 0
+                }
+                R.id.r_button_other_name -> {
+                    idRadioGroupOperator = 1
+                }
+                else -> {
+                }
+            }
+        }
+        radioGroupCustomer = view.findViewById(R.id.r_group_customer)
+        radioGroupCustomer?.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.r_button_name_of_customer -> {
+                    idRadioGroupCustomer = 0
+                }
+                R.id.r_button_another_name_of -> {
+                    idRadioGroupCustomer = 1
+                }
+                else -> {
+                }
+            }
+        }
+        radioGroupFarm = view.findViewById(R.id.r_group_farm)
+        radioGroupFarm?.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.r_button_farm_name -> {
+                    idRadioGroupFarm = 0
+                }
+                R.id.r_button_name_of_a_farm -> {
+                    idRadioGroupFarm = 1
+                }
+                else -> {
+                }
+            }
+        }
+        radioGroupField = view.findViewById(R.id.r_group_field)
+        radioGroupField?.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.r_button_field_name -> {
+                    idRadioGroupField = 0
+                }
+                R.id.r_button_name_of_a_field -> {
+                    idRadioGroupField = 1
+                }
+                R.id.r_button_field_n1 -> {
+                    idRadioGroupField = 2
+                }
+                else -> {
+
+                }
+            }
         }
     }
 
@@ -42,10 +172,25 @@ class StartFragment : Fragment() {
     }
 
     companion object {
-        fun newInstance() = StartFragment()
+        fun newInstance(
+            idFromMainOperator: Int,
+            idFromMainCustomer: Int,
+            idFromMainFarm: Int,
+            idFromMainField: Int
+        ): StartFragment {
+            val f = StartFragment()
+            val bundleFromMain = Bundle()
+            bundleFromMain.putInt("Operator", idFromMainOperator)
+            bundleFromMain.putInt("Customer", idFromMainCustomer)
+            bundleFromMain.putInt("Farm", idFromMainFarm)
+            bundleFromMain.putInt("Field", idFromMainField)
+            f.arguments = bundleFromMain
+            return f
+        }
     }
 }
 
 interface StartFragmentClickListener {
-    fun save()
+    fun save(idOperator: Int, idCustomer: Int, idFarm: Int, idField: Int)
 }
+
